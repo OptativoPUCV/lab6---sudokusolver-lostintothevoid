cc622a7b3636b464454a565d67ca874d75cf6612
Replit user <>
2023-05-05 04:39:20 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,13 +50,23 @@ int is_valid(Node* n){
 
 
 List* get_adj_nodes(Node* n){
-    List* list=createList();
-    return list;
+  List* list=createList();
+  int i, j;
+  for(i=0; i<9; i++){
+    for(j=0; j<9; j++){
+      if(n->sudo[i][j]==0){
+        Node* novo = copy(n);
+        novo->sudo[i][j]=1;
+        
+      }
+    }
+  }
+  return list;
 }
 
 
 int is_final(Node* n){
-    return 0;
+  return 0;
 }
 
 Node* DFS(Node* initial, int* cont){
